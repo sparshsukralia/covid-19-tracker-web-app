36b07474a7c452bd85f1c5e9b44614f92d182a7f
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-06-22 01:45:39 +0545

    all countries

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { fetchData } from "./api";
 class App extends React.Component {
   state = {
     data: {},
+    country: "",
   };
 
   async componentDidMount() {
@@ -14,13 +15,19 @@ class App extends React.Component {
     this.setState({ data: fetchedData });
   }
 
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country });
+  };
+
   render() {
     const { data } = this.state;
 
     return (
       <div className={styles.container}>
         <h1 className={styles.heading}>COVID-19 TRACKER</h1>
-        <CountryPicker />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Cards data={data} />
       </div>
     );
